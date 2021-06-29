6ec7c3ef4ead039a1b340591618a092137bf71cd
SmatMan <gamingajun10@gmail.com>
2021-06-29 09:40:38 -0400

    add basic barebones app indexing and saving to json

---
+++ appsearch.py
@@ -0,0 +1,23 @@
+import os
+from collections import OrderedDict
+import getpass
+import json
+
+def createindex(path=[r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs", rf"C:\Users\{getpass.getuser()}\AppData\Roaming\Microsoft\Windows\Start Menu"]):
+    rawFileList = {}
+    
+    for i in path:
+        for root, dirs, files in os.walk(i, followlinks=False):
+            for file in files:
+                print(file)
+                if file.endswith(".lnk") and not file.startswith("desktop"):
+                    rawFileList[file] = os.path.join(root, file)
+                    
+    filelist = OrderedDict(sorted(rawFileList.items(), key=lambda t: t[0]))
+    
+    return filelist
+
+def saveIndex(output="index.json"):
+    index = createindex()
+    with open(output, "w") as f:
+        f.write(json.dumps(index, indent=4))
