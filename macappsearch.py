ed1e08def39f383646980a0352220bfb70557372
SmatMan <gamingajun10@gmail.com>
2021-08-06 21:17:59 -0700

    add mac backend

---
+++ macappsearch.py
@@ -0,0 +1,29 @@
+import os
+from collections import OrderedDict
+import getpass
+import json
+from glob import glob
+
+path=["/Applications", "/System/Applications", f"/Users/{getpass.getuser()}/Applications"]
+rawFileList = {}
+
+def createIndex(path=path):
+    # For every filepath in path, find all the folders (no subdirectories) ending in .app and add them to the dict rawFileList
+    for i in path:
+        files = glob(f"{i}/*/")
+        for file in files:
+            if ".app" in file:
+            # Append every filepath along with the os.path.basename(path) to the dict rawFileList reverse. make sure the trailing slash and ".app" is removed before os.path.basename(path) is appended
+                rawFileList[os.path.basename(file[:-5])] = file[:-1]
+
+    # Create an ordered dict with the key being the name of the app and the value being the filepath
+    index = OrderedDict()
+    for key, value in rawFileList.items():
+        index[key] = value
+    return index
+
+
+def saveIndex(output="index.json"):
+    index = createIndex()
+    with open(output, "w") as f:
+        f.write(json.dumps(index, indent=4))
