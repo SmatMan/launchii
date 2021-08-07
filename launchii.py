30f2345e7b2357d9b18bddf79f65c19bfa13b807
SmatMan <gamingajun10@gmail.com>
2021-08-06 21:18:09 -0700

    add a basic "frontend"

--- launchii.py
+++ launchii.py
@@ -1,13 +1,21 @@
-import appsearch as app
+from types import resolve_bases
+import macappsearch as app
 import json
 
-searchterm = input('Search: ').lower()
-
-index = {}
-
-with open("index.json", "r") as f:
+with open("index.json", "r") as f: # load index
     index = json.load(f)
 
-for i in index:
-    if searchterm in str(i):
-        print(i)
+searchterm = input('Search: ').lower() # get search term
+
+results = {}
+
+for i in index: # iterate over index
+    if searchterm in index[i].lower(): # if search term is in index
+        # append i to results as key and index[i] as value
+        results[i] = index[i]
+
+if len(results) == 0: # if no results
+    print('No results found.')
+else:
+    for i in results:
+        print(i + ': ' + results[i]) # print results)
