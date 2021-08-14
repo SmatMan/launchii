d2a50cf31f6aa025b9c1cbc1c1376bbda4a41c17
gigahurt <478848+gigahurt@users.noreply.github.com>
2021-08-14 14:13:56 -0400

    Remove cli duplication
    Replace code in launchii with main method that delegates to cli
    Add main wrapper around cli.py code to call from launchii
    Remove unused import

--- launchii.py
+++ launchii.py
@@ -1,41 +1,7 @@
-from types import resolve_bases
-import macappsearch as app
-import json
-import os
-from colours import *
+import cli
 
-with open("index.json", "r") as f: # load index
-    index = json.load(f)
+def main():
+    cli.main()
 
-searchterm = input(cyan('Search: ')).lower() # get search term
-
-
-while True:
-    results = app.searchIndex(index, searchterm) # search index for term
-
-    if len(results) == 0: # if no results
-        print('No results found.')
-        break
-    else:
-        # print every item from results with a number in front from 1-n, maximum 10
-        for i, item in enumerate(results):
-            print(str(i+1) + '. ' + yellow(str(item)) + '\n')
-        # ask user to select result and get index
-        option = input(cyan('Select result or start new search: '))
-        # check if option is a number
-        try:
-            option = int(option)
-            result = results[list(results.keys())[option-1]]
-            # ask user if they want to open the app, if yes, open app using os.system("open")
-            shouldOpen = input(f"Type {yellow('y')} to open {green(str(list(results.keys())[option-1]))}, or {yellow('n')} to quit: ")
-            
-            if shouldOpen == 'y':
-                os.system("open " + result)
-                break
-            else:
-                break
-        except ValueError:
-            searchterm = option
-
-        
-        
+if __name__ == "__main__":
+    main()
